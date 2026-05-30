1150f041f86f1b44c4c887fffeb02fbd5ee6166d
ram0552 <rambabu.ghantasala@sasi.ac.in>
2026-05-30 18:59:04 +0530

    Updated SMTP configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,33 +1,23 @@
 import nodemailer from "nodemailer";
 
-const sendEmail = async (
-  options
-) => {
-
+const sendEmail = async (options) => {
   const transporter =
     nodemailer.createTransport({
-      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
 
       auth: {
-        user:
-          process.env.EMAIL_USER,
-
-        pass:
-          process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
   await transporter.sendMail({
-    from:
-      process.env.EMAIL_USER,
-
+    from: process.env.EMAIL_USER,
     to: options.email,
-
-    subject:
-      options.subject,
-
-    html:
-      options.message,
+    subject: options.subject,
+    html: options.message,
   });
 };
 
